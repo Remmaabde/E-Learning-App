1958e20aca287e69fde3477f5eb9b4138458c805
Nutnell <160223221+Nutnell@users.noreply.github.com>
2025-08-25 09:03:02 +0300

    Removed repeated lines

--- frontend/src/services/quizService.ts
+++ frontend/src/services/quizService.ts
@@ -1,25 +1,11 @@
 import axios from "axios";
-<<<<<<< HEAD
-import API_BASE_URL from "../config/api";
 import type { Quiz } from "../Pages/courses/types";
 
-
-export const fetchQuizByLesson = async (lessonId: string) => {
-  const res = await axios.get<Quiz>(`${API_BASE_URL}/quizzes/${lessonId}`);
-  return res.data;
-};
-
-export const submitQuizAttempt = async (quizId: string, answers: { questionId: string, answer: string }[]) => {
-  const res = await axios.post(`${API_BASE_URL}/quizzes/attempts`, { quizId, answers });
-  return res.data;
-};
-=======
-import type { Quiz } from "../Pages/courses/types";
-
-const API_URL = "http://localhost:5000/api/quizzes";
+const API_URL = "http://localhost:5000/api";
 
 export async function fetchQuizByLesson(lessonId: string): Promise<Quiz> {
-  const res = await axios.get(`${API_URL}/${lessonId}`, { withCredentials: true });
+ 
+  const res = await axios.get(`${API_URL}/quizzes/${lessonId}`, { withCredentials: true });
   return res.data;
 }
 
@@ -27,7 +13,7 @@ export async function submitQuizAttempt(
   quizId: string,
   answers: { questionId: string; answer: string }[]
 ) {
-  const res = await axios.post("http://localhost:5000/api/quiz-attempts", { quizId, answers }, { withCredentials: true });
+
+  const res = await axios.post(`${API_URL}/quiz-attempts`, { quizId, answers }, { withCredentials: true });
   return res.data;
 }
->>>>>>> 41da9a51bd70727d9f697788e63200d361fe5223
